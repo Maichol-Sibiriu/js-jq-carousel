ed676c24518f9dfdbdbf01c2e9ad5017092e9eff
Maichol <msibiriu@gmail.com>
2020-10-27 18:47:52 +0100

    ok anche parte prev

--- main.js
+++ main.js
@@ -20,8 +20,26 @@ function direct(direction) {
    img.removeClass("active");
    ico.removeClass("active");
 
-   if (direction === "next") {
-      img.next("img").addClass("active");
-      ico.next("i").addClass("active");
-   }
+    if (direction === "next") {
+
+      if (img.hasClass("last") ) {
+        $(".images .first").addClass("active");
+        $(".nav .first").addClass("active");
+      }
+      else {
+       img.next("img").addClass("active");
+       ico.next("i").addClass("active");
+      }
+
+    }
+    else if (direction === "prev") {
+      if (img.hasClass("first") ) {
+        $(".images .last").addClass("active");
+        $(".nav .last").addClass("active");
+      }
+      else {
+       img.prev("img").addClass("active");
+       ico.prev("i").addClass("active");
+      }
+    }
 };
