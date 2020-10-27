f8b39c9113377804cab5952eddbecb301aaed53a
Maichol <msibiriu@gmail.com>
2020-10-27 16:37:37 +0100

    fatto parte next

--- main.js
+++ main.js
@@ -2,6 +2,26 @@
 
 $(document).ready( function() {
 
+   $(".prev").click( function() {
+      direct(".prev");
+   });
 
+   $(".next").click( function() {
+      direct(".next");
+   });
 
 });
+
+// direzione click
+function direct(direction) {
+   var img = $(".images .active");
+   var ico = $(".nav .active");
+
+   img.removeClass(".active");
+   ico.removeClass(".active");
+
+   if (direction === ".next") {
+      img.next("img").addClass(".active");
+      ico.next("i").addClass(".active");
+   }
+};
